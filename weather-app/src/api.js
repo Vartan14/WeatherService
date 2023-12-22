7cb8f5cda75722f73efdc7913bbafe93c6400644
Карамян Вартан Суренович <89924019+Vartan14@users.noreply.github.com>
2023-12-22 19:56:06 +0200

    add search bar and dummy current weather tab (#1)

---
+++ weather-app/src/api.js
@@ -0,0 +1,11 @@
+export const geoApiOptions = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': 'fb9d6d0b5cmsh22f792604adf160p1d777ejsn4072c86c27b9',
+		'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+	}
+};
+
+export const GEO_API_URL = 'https://wft-geo-db.p.rapidapi.com/v1/geo';
+
+
